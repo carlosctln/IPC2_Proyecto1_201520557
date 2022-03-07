6a7acb488665a095b80897eef8039ccc87219d30
Carlos Catalan <catalanc12@gmail.com>
2022-03-06 21:11:35 -0600

    implementando graphviz

--- Matriz.py
+++ Matriz.py
@@ -1,4 +1,5 @@
 from nodoOr import NodoOr
+import os
 
 class MatrizOrtogonal:
     def __init__(self):
@@ -48,3 +49,29 @@ class MatrizOrtogonal:
                         aux = aux.anterior
         img = img+aux.dato
         return img
+
+    def Graficar(self):
+        aux = self.cabeza
+        cont = 0
+        cadena  =''
+        file = open('grafica.dot', 'w')
+        cadena = cadena + 'graph G {\n'
+        while (aux.abajo!=None) | (aux.siguiente != None):
+            cadena = cadena + 'Node'+str(cont)+'[label=\"'+aux.dato+'\"];\n'
+            if aux.siguiente !=None:
+                aux = aux.siguiente
+                cadena = cadena + 'Node'+str(cont + 1)+'[label=\"'+aux.dato+'\"];\n'
+                cont += 1 
+            else:
+                cadena = cadena + 'Node'+str(cont + 1)+'[label=\"'+aux.dato+'\"];\n'
+                if aux.abajo!=None:
+                    aux = aux.abajo
+                    cadena = cadena + 'Node'+str(cont)+'[label=\"'+aux.dato+'\"];\n'
+                    while aux.anterior!=None:
+                        aux = aux.anterior                       
+                cont += 1
+        cadena = cadena + '}'
+        file.write(cadena)
+        file.close()
+        os.system('dot -Tpng grafica.dot -o grafica.png')
+        os.startfile("grafica.png")
